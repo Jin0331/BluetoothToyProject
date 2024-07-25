c2faa30adb5ca28af99e57a361a5b3b31f7ddb3b
Jin0331 <sempre813@naver.com>
2024-07-25 13:19:32 +0900

    Initial Commit

---
+++ BluetoothToyProject/BluetoothToyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothToyProjectApp.swift
+//  BluetoothToyProject
+//
+//  Created by JinwooLee on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothToyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothToyProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BluetoothToyProject
+//
+//  Created by JinwooLee on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
