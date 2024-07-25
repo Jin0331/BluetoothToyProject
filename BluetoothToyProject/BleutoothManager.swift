de9d693e776db3a2f6c557ee3ac9e33490463ee2
Jin0331 <sempre813@naver.com>
2024-07-25 14:52:51 +0900

    [Add] CoreBluetooth Manager 추가

---
+++ BluetoothToyProject/BleutoothManager.swift
@@ -0,0 +1,44 @@
+//
+//  BleutoothManager.swift
+//  BluetoothToyProject
+//
+//  Created by JinwooLee on 7/25/24.
+//
+
+import CoreBluetooth
+
+class BluetoothManager: NSObject, CBCentralManagerDelegate, ObservableObject {
+    @Published var isBluetoothEnabled = false
+    @Published var discoveredPeripherals = [CBPeripheral]()
+
+    private var centralManager: CBCentralManager!
+
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            isBluetoothEnabled = true
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            isBluetoothEnabled = false
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !discoveredPeripherals.contains(peripheral) {
+            discoveredPeripherals.append(peripheral)
+        }
+    }
+
+    func toggleBluetooth() {
+        if centralManager.state == .poweredOn {
+            centralManager.stopScan()
+            centralManager = nil
+        } else {
+            centralManager = CBCentralManager(delegate: self, queue: nil)
+        }
+    }
+}
